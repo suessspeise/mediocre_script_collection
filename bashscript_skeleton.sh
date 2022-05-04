9694e2d66755bc2057d117c7512338786d5d627b
h_campos <github@camposcampos.de>
2022-05-04 15:26:13 +0200

    Create bashscript_skeleton.sh

---
+++ bashscript_skeleton.sh
@@ -0,0 +1,26 @@
+# function intended to be added to .bashrc or .profile
+skeleton(){
+        FILE_PATH="$1.sh"
+        echo '' > ${FILE_PATH}
+        cat >> "${FILE_PATH}" << EOF
+#!/bin/bash
+#SBATCH --partition=compute
+#SBATCH --account=mh0926
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=02:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=hernan.campos@mpimet.mpg.de
+#SBATCH --output=log/%x.%j.log
+#SBATCH --error=log/%x.%j.log
+
+
+run() {
+        echo "this is a generated bash script skeleton"
+}
+
+run \$@
+        
+EOF
+}
+skeleton $1
